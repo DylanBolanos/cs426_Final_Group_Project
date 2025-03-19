cc18d5d69376a6127a29c7e3282dd81cadf18a08
Dylan Bolanos <ryuyadylan@gmail.com>
2025-03-18 22:33:09 -0500

    level change

---
+++ Desktop/All code/unity/cs426_Final_Group_Project/Assets/Scripts/Glass.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public class Glass : MonoBehaviour, IInteractable
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public void Interact()
+    {
+        PlayerMovement player = GameObject.FindWithTag("Player").GetComponent<PlayerMovement>();
+
+        if (player.heldGlass == null)
+        {
+            player.heldGlass = this.gameObject;
+            transform.SetParent(player.transform);
+            transform.localPosition = new Vector3(0.5f, 0.3f, 1f);
+            transform.localRotation = Quaternion.identity;
+            GetComponent<Rigidbody>().isKinematic = true;
+            Debug.Log("Player Holds a glass");
+        }
+    }
+    
+    void Start()
+    {
+    }
+}
---
+++ Desktop/All code/unity/cs426_Final_Group_Project/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,122 @@
+using UnityEngine;
+using System.Collections.Generic;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed = 10f;
+    public float rotationSpeed = 100f;
+    public float force = 300f;
+    public Transform cameraTransform;
+    public float mouseSensitivity = 2.0f;
+    private float cameraRotationX = 0f;
+    public static bool isused = false;
+    bool canJump = true;
+    // glass
+    private IInteractable nearbyInteractable = null;
+    private float interactionDistance = 0f;
+    private Transform nearbyObjectTransform; 
+
+    public GameObject heldGlass = null;
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    Rigidbody rb;
+    Transform t;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        t = GetComponent<Transform>();
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity;
+        cameraRotationX -= mouseY;
+        cameraRotationX = Mathf.Clamp(cameraRotationX, -90f, 90f);
+        cameraTransform.localRotation = Quaternion.Euler(cameraRotationX, 0f, 0f);
+        
+        if (Input.GetKey(KeyCode.W))
+            rb.linearVelocity += this.transform.forward * speed * Time.deltaTime;
+        else if (Input.GetKey(KeyCode.S))
+            rb.linearVelocity -= this.transform.forward * speed * Time.deltaTime;
+
+        //rotation
+        if (Input.GetKey(KeyCode.D))
+            t.rotation *= Quaternion.Euler(0, rotationSpeed * Time.deltaTime, 0);
+        else if (Input.GetKey(KeyCode.A))
+            t.rotation *= Quaternion.Euler(0, - rotationSpeed * Time.deltaTime, 0);
+        
+        if (Input.GetKeyDown(KeyCode.Space) && canJump)
+        {
+            rb.AddForce(t.up * force);
+            canJump = false;
+            StartCoroutine(ResetJump());
+        }
+
+        if(Input.GetKeyDown(KeyCode.F)){
+            TryInteract();
+        }
+        if(Input.GetKeyDown(KeyCode.G)){
+            DropGlass();
+        }
+    }
+    IEnumerator ResetJump()
+    {
+        yield return new WaitForSeconds(1.5f);
+        canJump = true;
+    }
+
+    private void TryPickUpGlass()
+    {
+        Collider[] hitColliders = Physics.OverlapSphere(transform.position, 2f);
+        foreach (Collider collider in hitColliders)
+        {
+            if (collider.CompareTag("Glass"))
+            {
+                heldGlass = collider.gameObject;
+                heldGlass.transform.SetParent(this.transform);
+                heldGlass.transform.localPosition = new Vector3(0.5f, -0.5f, 1f);
+                heldGlass.GetComponent<Rigidbody>().isKinematic = true;
+                Debug.Log("Player Picked up glass");
+                return;
+            }
+        }
+    }
+
+    private void DropGlass()
+    {
+        heldGlass.transform.SetParent(null);
+        heldGlass.GetComponent<Rigidbody>().isKinematic = false;
+        heldGlass = null;
+        Debug.Log("Player Dropped off Glass");
+    }
+    
+    private void TryInteract(){
+        if (nearbyInteractable != null)
+        {
+            float distanceToObject = Vector3.Distance(transform.position, nearbyObjectTransform.position);
+            if (distanceToObject <= interactionDistance)
+            {
+                if (nearbyObjectTransform.CompareTag("Experiment") && heldGlass == null)
+                {
+                    Debug.Log("Need to Hold a Glass");
+                }
+                else
+                {
+                    nearbyInteractable.Interact();
+                }
+            }
+            else
+            {
+                Debug.Log("Too far");
+            }
+        }
+        else
+        {
+            TryPickUpGlass();
+        }
+    }
+}
